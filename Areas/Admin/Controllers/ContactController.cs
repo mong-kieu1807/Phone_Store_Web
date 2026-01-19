2489d0faf0180e06d625f5eecf9dc5a953e1b750
mong-kieu1807 <0306231397@caothang.edu.vn>
2026-01-19 21:14:28 +0700

    Chỉnh sửa thêm Quản lý liên hệ vào admin

---
+++ Areas/Admin/Controllers/ContactController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PhoneStore.Areas.Admin.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: ContactController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
